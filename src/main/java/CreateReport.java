fa08d7d533fccf147b836ad676b9c88088d09190
rancdg <rancdg@gmail.com>
2015-08-01 18:36:08 +0300

    circles fixed + annotations

--- src/main/java/CreateReport.java
+++ src/main/java/CreateReport.java
@@ -3,6 +3,12 @@ import com.google.gson.reflect.TypeToken;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.annotations.XYAnnotation;
+import org.jfree.chart.annotations.XYImageAnnotation;
+import org.jfree.chart.annotations.XYTextAnnotation;
+import org.jfree.chart.event.PlotChangeEvent;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.xy.*;
 
 import javax.imageio.ImageIO;
@@ -21,11 +27,11 @@ public class CreateReport {
     int iExitValue;
     String sCommandString;
 
-    public static final String CSV_FILES_PATH = "/WWH-results/app/resources/outputs";
-    public static final String DB_PROPERTIES_PATH = "/WWH-results/app/resources/dbproperties.json";
-    public static final String GLOBAL_REUDCER_RESULTS = "/WWH-results/app/resources/part-r-00000 ";
+    public static final String CSV_FILES_PATH = "/home/ran/WWH-results/app/resources/outputs";
+    public static final String DB_PROPERTIES_PATH = "/home/ran/WWH-results/app/resources/dbproperties.json";
+    //public static final String GLOBAL_REUDCER_RESULTS = "~/WWH-results/app/resources/part-r-00000 ";
 
-    public static final String SERVER_PATH = "/WWH-results/app";
+    public static final String SERVER_PATH = "/home/ran/WWH-results/app";
 
 
     private void drawOutput(String csvsPath) throws FileNotFoundException {
@@ -57,26 +63,24 @@ public class CreateReport {
                         String newFileOutPath = outGenePath+"/"+filename;
                         //create and print the chart to jpeg
                         JFreeChart chart = ChartFactory.createStackedXYAreaChart("Coverage Plot" , "Position" , "Base Count" , dataSet);
+                        chart.addSubtitle(new TextTitle("Depth- " + parameters[0].substring(0,parameters[0].length()-9) + ". Coverage- " + parameters[1].substring(0,parameters[1].length()-9) + "%. num of reads- "+ parameters[3] + ". Integral- "+ parameters[2]));
                         try {
                             ChartUtilities.saveChartAsJPEG(new File(newFileOutPath+"_hist.jpeg"), chart, 500, 300);
                         } catch (IOException e) {
                             System.err.println("Problem occurred creating chart.");
                         }
 
+                        int depth = (int) Math.round(Double.parseDouble(parameters[0])*100);
                         //Draw the depth circle
                         // Open a JPEG file, open a new buffered image
                         File imgF = new File(newFileOutPath+"_circ.jpeg");
-                        BufferedImage img = new BufferedImage(1000,1000 , BufferedImage.TYPE_INT_RGB);
-
+                        BufferedImage img = new BufferedImage(depth+10 , depth+10 , BufferedImage.TYPE_INT_RGB);
 
                         // Obtain the Graphics2D context associated with the BufferedImage.
                         Graphics2D g = img.createGraphics();
-                        int depth = (int) Math.round(Double.parseDouble(parameters[0])*100);
-                        g.setColor(new Color(100,100,100));
-                        g.draw(new Ellipse2D.Double(0,0,100,100));
-                        //g.fillOval(1000, 1000, depth, depth);
-
-
+                        g.setColor(new Color(77,153,255));
+                        g.setBackground(new Color(255, 255, 204));
+                        g.fillOval(5, 5, depth, depth);
                         ImageIO.write(img, "jpeg", imgF);
 
                     } catch (FileNotFoundException e) {
@@ -170,7 +174,7 @@ public class CreateReport {
     }
 
     public static void main(String[] args) throws IOException {
-        HistogramParser.parse(GLOBAL_REUDCER_RESULTS, CSV_FILES_PATH);
+        //HistogramParser.parse(GLOBAL_REUDCER_RESULTS, CSV_FILES_PATH);
         CreateReport createReport = new CreateReport();
         createReport.drawOutput(CSV_FILES_PATH);
         List<DB> dbs = createReport.createDBobj(CSV_FILES_PATH);
